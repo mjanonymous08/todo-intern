a6a7e4172b93cf71d654eb1b95cf2153a49e8c69
humblebee05 <hamdallahogunpola@gmail.com>
2023-09-07 17:58:57 +0100

    uploas

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,18 +1,27 @@
 import {GrAdd} from 'react-icons/gr'
 import CreateNew from './section/CreateNew'
-const MainNav = () => {
+import { useState } from 'react'
+ 
+const MainNav = ({create}) => {
+const [createnew, setcreatenew] = useState(false) 
   return (
     <div className='px-10 relative py-5'>
         <div className='flex justify-between'>
             <p className='text-lg'>todo</p>
-            <button className='font-bold text-2xl animate-pulse'><GrAdd /></button>
+            <button onClick={() => setcreatenew(!createnew)}  className='font-bold text-2xl animate-pulse'><GrAdd /></button>
+            
         </div>
-        <div className='absolute opacity-50 top-0 left-0 w-[100%] items-center h-[90vh] flex justify-center  bg-white '>
-          
-        </div>
-        <div className='absolute top-28 left-64'>
-          <CreateNew />
-        </div>
+        { 
+          createnew && 
+          <div className='absolute opacity-50 top-0 left-0 w-[100%] items-center h-[90vh] flex justify-center  bg-white '>
+
+        </div>}
+        {
+          createnew && 
+            <div className='absolute top-28 left-64'>
+              <CreateNew create={create} open={createnew} setopen={setcreatenew}  />
+        </div>}
+
     </div>
   )
 }
--- components/section/Entertainment.jsx
+++ components/section/Entertainment.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {AiOutlineEllipsis} from 'react-icons/ai'
 
 const Entertainment = () => {
